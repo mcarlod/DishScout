d8913917cd885d21e77bb489e53796b5f96091b3
mcarlod <carlo.diesta@gmail.com>
2026-07-03 17:26:36 +0930

    Implemented first draft of styling for navigation bar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from '../assets/DishScoutLogo.png'
+import '../styles/components.css'
 
 function Navbar() {
     return (
-        <nav>
-            <div>
-                <img src={logo} alt="DishScout Logo" />
+        <nav className = "navbar">
+            <div className="logo-container">
+                <img src={logo} alt="DishScout Logo" className="logo"/>
                 <h2>DishScout</h2>
             </div>
 
-            <div>
+            <div className="nav-pages">
                 <button onClick={() => navigate("/home")}>Recipes</button>
                 <button onClick={() => navigate("/saved")}>Saved</button>
                 <button onClick={() => navigate("/profile")}>Profile</button>
