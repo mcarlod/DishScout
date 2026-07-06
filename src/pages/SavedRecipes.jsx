b6d265345e64f494cedfd507d30dfdb1de4bf637
mcarlod <carlo.diesta@gmail.com>
2026-07-06 17:00:48 +0930

    Implemented first iteration of saved recipes page.

--- src/pages/SavedRecipes.jsx
+++ src/pages/SavedRecipes.jsx
@@ -1,5 +1,29 @@
+import Navbar from '../components/Navbar'
+import RecipeCard from "../components/RecipeCard.jsx";
+import pepperpot from "../assets/Rectangle 5.png"
+
 function SavedRecipes() {
-    return <h1>Saved Page</h1>
+    return(
+        <>
+            <header>
+                <Navbar />
+            </header>
+
+            <main>
+                <h1>Your Saved Recipes</h1>
+
+                <section>
+                    <RecipeCard
+                        image={pepperpot}
+                        recipeName="Barbados Pepperpot"
+                        country="Guyana"
+                        prepTime={45}
+                        servings={4}
+                    />
+                </section>
+            </main>
+        </>
+    );
 }
 
 export default SavedRecipes;
