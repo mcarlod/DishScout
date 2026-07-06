f77964107d5485ec942de9349f7cdd2faf9e2337
mcarlod <carlo.diesta@gmail.com>
2026-07-06 18:01:45 +0930

    Removed Navbar code as it is related to Layout file.

--- src/pages/SavedRecipes.jsx
+++ src/pages/SavedRecipes.jsx
@@ -1,27 +1,20 @@
-import Navbar from '../components/Navbar'
 import RecipeCard from "../components/RecipeCard.jsx";
 import pepperpot from "../assets/Rectangle 5.png"
 
 function SavedRecipes() {
     return(
         <>
-            <header>
-                <Navbar />
-            </header>
+            <h1>Your Saved Recipes</h1>
 
-            <main>
-                <h1>Your Saved Recipes</h1>
-
-                <section>
-                    <RecipeCard
-                        image={pepperpot}
-                        recipeName="Barbados Pepperpot"
-                        country="Guyana"
-                        prepTime={45}
-                        servings={4}
-                    />
-                </section>
-            </main>
+            <section>
+                <RecipeCard
+                    image={pepperpot}
+                    recipeName="Barbados Pepperpot"
+                    country="Guyana"
+                    prepTime={45}
+                    servings={4}
+                />
+            </section>
         </>
     );
 }
